aca92b051c8c4367b164462281ed85188982861b
guscao <113106462+guscao@users.noreply.github.com>
2024-10-25 10:40:27 -0300

    pepa

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,18 @@
+
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGlobais() {
+     const res = await fetch(url)
+     const dados = await res.json()
+     console.log(dados);
+     const paragrafo = document.createElement('p')
+     paragrafo.classList.add('graficos-container__texto')
+
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente`
+    console.log(paragrafo)
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+}
+
+visualizarInformacoesGlobais()
